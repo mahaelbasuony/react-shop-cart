3c160a6bf82ab8d6112f8e600b4275315c6a828e
Maha Elbasuony <mahaelbasuony@gmail.com>
2021-11-15 23:20:06 +0100

    create cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import NavBar from "./components/navbar";
+import "./App.css";
+import Counters from "./components/counters";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  constructor() {
+    super();
+    console.log("app- constuctor");
+  }
+
+  componentDidMount() {
+    console.log("mount");
+  }
+  handelRest = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handelDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+  handelIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <div>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onDelete={this.handelDelete}
+            onIncrement={this.handelIncrement}
+            onReset={this.handelRest}
+          ></Counters>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
